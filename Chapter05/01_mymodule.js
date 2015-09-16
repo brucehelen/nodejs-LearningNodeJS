95d0d9aeb3b69a7a9fb3088dac82ee932882b073
MissionHealth <missionhealth@MissionHealthdeMac-mini.local>
2015-09-16 09:28:19 +0800

    init commit

---
+++ Chapter05/01_mymodule.js
@@ -0,0 +1,26 @@
+
+
+function Greeter (lang) {
+    this.language = lang;
+    this.greet = function () {
+        switch (this.language) {
+        case "en": return "Hello!";
+        case "de": return "Hallo!";
+        case "jp": return "こんにちは!";
+        default: return "No speaka that language";
+        }
+    }
+    console.log(this.greet());
+}
+
+exports.hello_world = function () {
+    console.log("Hello World");
+}
+
+exports.goodbye = function () {
+    console.log("Bye bye!");
+}
+
+exports.greeter = function (lang) {
+    return new Greeter(lang);
+}
---
+++ Chapter05/test.js
@@ -0,0 +1,9 @@
+
+
+var mm = require('./01_mymodule.js');
+
+mm.hello_world();
+var ge = mm.greeter;
+ge("jp");
+
+mm.goodbye();
